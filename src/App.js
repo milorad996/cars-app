10bec10df733a46a12ba8a7755d19e076278158f
milorad996 <mips96.1@gmail.com>
2022-06-27 21:11:11 +0200

    Zadatak 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
 import './App.css';
+import AppCars from './pages/AppCars';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/cars">Cars</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/cars">
+            <AppCars />
+          </Route>
+
+        </Switch>
+      </Router>
+
     </div>
   );
 }
---
+++ src/pages/AppCars.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+function AppCars() {
+    return (
+        <h1>AppCars</h1>
+    )
+}
+
+export default AppCars;
