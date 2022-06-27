801aa0cd008eaf2ffe1d78c5c8fbb4fee9d4be7c
milorad996 <mips96.1@gmail.com>
2022-06-27 21:35:01 +0200

    Zadatak 2

--- src/pages/AppCars.js
+++ src/pages/AppCars.js
@@ -1,8 +1,32 @@
 import React, { useState } from "react";
-
+import CarService from "../services/CarService";
+import axios from 'axios';
 function AppCars() {
+
+
+    const [cars, setCars] = useState([])
+
+    async function getData() {
+        try {
+            const cars = await CarService.getAll()
+
+            //setPosts(cars)
+            console.log(cars)
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
-        <h1>AppCars</h1>
+
+        <div className="App">
+            <h1>AppCars</h1>
+
+
+            <ol>
+                {cars.map((car) => <li key={car.id}>{car.brand}</li>)}
+            </ol>
+            <button onClick={getData}>Dovuci podatke</button>
+        </div>
     )
 }
 
--- src/services/CarService.js
+++ src/services/CarService.js
@@ -1,5 +1,22 @@
 
 
-class CarServices {
+import axios from 'axios'
 
+class CarService {
+    constructor() {
+        this.httpClient = axios.get('http://localhost:3000/explorer');
+
+    }
+
+    async getAll() {
+        try {
+            const { data } = await this.httpClient.get('/api/cars')
+
+            return data;
+        } catch (error) {
+            return []
+        }
+    }
 }
+
+export default new CarService()
